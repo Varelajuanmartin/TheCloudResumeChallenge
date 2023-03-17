f07a84f261259b01b4d0d00362786499b193e4d5
varelajuanmartin <varelajuanmartin@gmail.com>
2023-03-17 11:42:48 -0300

    Added a lambda2.js that reads the value of the dynamoDB table and incresases it by one

---
+++ lambda2.js
@@ -0,0 +1,31 @@
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB();
+
+exports.handler = async (event) => {
+  const pageName = "1";
+  
+  // Construct the update parameters
+  const params = {
+    TableName: 'VisitorCount',
+    Key: {
+      'PageName': { S: pageName }
+    },
+    UpdateExpression: 'SET #count = #count + :incr',
+    ExpressionAttributeNames: {
+      '#count': 'Count'
+    },
+    ExpressionAttributeValues: {
+      ':incr': { N: '1' }
+    },
+    ReturnValues: 'UPDATED_NEW'
+  };
+  
+  // Perform the update operation
+  const result = await dynamodb.updateItem(params).promise();
+  const count = result.Attributes.Count.N;
+  
+  return {
+    statusCode: 200,
+    body: `Visitor count updated to ${count}`
+  };
+};
